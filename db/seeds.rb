7e48144dc076ff68fb398636cce8034d59aad5c7
rails udemy course <karimbahi@gmail.com>
2020-03-06 18:27:48 +0000

    chatroom implemented without websocket and actioncable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "mariem", password: "password")
+User.create(username: "sonia", password: "password")
+User.create(username: "azza", password: "password")
+User.create(username: "rached", password: "password")
+User.create(username: "ferid", password: "password")
